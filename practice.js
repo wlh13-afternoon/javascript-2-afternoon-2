359d5dbc5f198b15d0fd10b047dc9c6a0ffc7ff9
Robert Knowles <robertknowles@Roberts-MacBook-Pro.local>
2019-10-31 11:13:54 -0600

    part 2 regular problems completed

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr) {
+  return arr[0]
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last (arr){
+  return arr[arr.length - 1]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -49,8 +53,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
-
+function looper (family) {
+  for (let i = 0; i < family.length; i++) {
+    alert(family[i])
+  }
+}
+// looper(family)
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -64,7 +72,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-
+function reversedLooper(letters) {
+  for (let i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i])
+  }
+}
+// reversedLooper(letters)
 
 ////////// PROBLEM 5 //////////
 
@@ -79,7 +92,17 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
+function evenFinder(nums){
+  let newArr = []
+  for (let i = 0; i < nums.length; i++){
+     if (nums[i] % 2 === 0 && nums[i] !== 0) {
+       newArr.push(nums[i])
+     }
+    }
+    console.log(newArr)
+    return newArr
+}
+console.log(evenFinder(nums))
 
 
 
